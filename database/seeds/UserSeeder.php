246a6a039775c9e32a02940c6dadb8b06a119549
Apolinar <apolinar.morales@corporativopmn.com>
2020-05-03 17:32:28 -0500

    Modification the user sedder to heroku

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\User;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -13,7 +15,8 @@ class UserSeeder extends Seeder
     public function run()
     {
         factory(User::class, 50)->create();
-        User::create(['name' => 'Apolinar Morales', 'email' => 'apolinar.morales.a@gmail.com', 'password' => '123123']);
+        $password = Hash::make('password');
+        User::create(['name' => 'Apolinar Morales', 'email' => 'apolinar.morales.a@gmail.com', 'email_verified_at' => Carbon::now(), 'password' => $password,]);
         /* if (App::isLocal()) {
             factory(User::class, 50)->create();
             User::create(['name' => 'Apolinar Morales', 'email' => 'apolinar.morales.a@gmail.com', 'password' => '123123']);
